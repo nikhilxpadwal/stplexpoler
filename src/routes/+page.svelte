4320e24000b45678d28f599fe5a2a9a40b894e5f
Your Name <you@example.com>
2023-01-10 09:51:56 +0530

    #3

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Navbar from '../components/Navbar.svelte'
+    import MainBody from '../components/MainBody.svelte'
+    import '../app.css'
+    
+</script>
+<div class="mx-auto">
+<Navbar />
+<MainBody />
+</div>
+<style global>
+    :global(body) {
+        background:#E5E5E5;
+        
+    }
+    :global(*)
+    {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#1A1A1A',
+      'secondary': '#0F89FF',
+      'textp': '#767676',
+      'accent':'#E4E4E4',
+    },
+    
+    
+  },
+  plugins: []
+};
